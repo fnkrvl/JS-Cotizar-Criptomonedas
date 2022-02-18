9b0b95307dc9dbaa0ec79f8dfd9ead9bcdb409f1
Franco Vicentin <franco.rvl91@gmail.com>
2022-02-18 09:35:34 -0300

    Construct selection form

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,29 @@
 class Interfaz{
     
+    constructor() {
+        this.init()
+    }
+    init() {
+        this.construirSelect()
+    }
+
+    construirSelect() {
+        cotizador.obtenerMonedasAPI()
+            .then(monedas => {
+                // crear un select de opciones
+                const select = document.querySelector('#criptomoneda')
+
+                // iterar por los resultadoas de la api
+                for( const [key, value] of Object.entries(monedas,monedas.Data)) {
+                    // añadir el symbol y el nombre como opciones
+                    const opcion = document.createElement('option')
+                    opcion.value = value.Symbol
+                    opcion.appendChild(document.createTextNode(value.CoinName))
+                    select.appendChild(opcion)
+                }
+            })
+    }
+
     mostrarMensaje(mensaje, clases) {
         const div = document.createElement('div')
         div.className = clases
@@ -14,7 +38,10 @@ class Interfaz{
             document.querySelector('.mensajes').remove()
         }, 3000);
     }
-    
 
+    // imprime el resultado de la cotizacion
+    mostrarResultado(resultado, moneda, crypto) {
+
+    }
 
 }
